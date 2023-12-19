1b911b7026b75b46d434f1148b034a0de958534c
kevin carbajal <ruizcarbajalalankevin@gmail.com>
2023-12-19 16:33:18 -0600

    hice el html y el codigo de javasript de todo lo que se ve en ello

---
+++ click.js
@@ -0,0 +1,36 @@
+const cardButtons = document.querySelectorAll('.card button');
+const modalOuter = document.querySelector('.modal-outer');
+const modalInner = document.querySelector('.modal-inner');
+
+function handlecardButtonClick(event) {
+    const button = event.currentTarget;
+    const card = button.closest('.card');
+    const imgSrc = card.querySelector('img').src;
+    const desc = card.dataset.description;
+    const name = card.querySelector('h2').textContent;
+    modalInner.innerHTML = `"<img width="600" height="600" src=${imgSrc.replace('200', '600')}"alt="${name}"/>
+    <p>${desc}</p>
+    `;
+    modalOuter.classList.add('open')
+ }
+
+cardButtons.forEach(button =>
+     button.addEventListener('click', handlecardButtonClick));
+
+     function closeModal(){
+        modalOuter.classList.remove('open');
+     }
+     modalOuter.addEventListener('click', function(event){
+       const isOutside = !event.target.closest('.modal-inner');
+       if (isOutside){
+        closeModal();
+       }
+
+     });
+     window.addEventListener('keydown', event => {
+        console.log(event);
+        if(event.key === 'Escape') {
+            closeModal();
+        }
+     });
+     
